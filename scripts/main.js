58aafee4dbf02db14d40b897a9f081f79ed101ef
rtxrulez <>
2020-01-13 21:56:58 +0300

    меню скрола

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,8 @@
 $(function() {
   var $body = $("body");
+  var $html = $("html");
+  var $header = $(".header");
+  var mobileWidth = 1060;
 
   $body.on("click", ".button-map", function() {
     $(".section11").toggleClass("map-open");
@@ -22,5 +25,16 @@ $(function() {
       tCounter:
         '<span class="mfp-counter-one">%curr%</span><span class="mfp-counter-separate"></span><span class="mfp-counter-all">%total%</span>'
     }
+  });
+
+  var docPos = $(document).scrollTop();
+
+  $(window).on("scroll", function() {
+    var fromTop = $(document).scrollTop();
+
+    if (docPos > fromTop) {
+      $body.toggleClass("header-fix", fromTop > 200);
+    }
+    docPos = fromTop;
   });
 });
